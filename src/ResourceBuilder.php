981766bdf421d0faf0b6fbb73924942e297ea668
carlo <carlo.nicora@gmail.com>
2022-02-07 15:26:52 +0100

    Cache added

--- src/ResourceBuilder.php
+++ src/ResourceBuilder.php
@@ -3,6 +3,9 @@ namespace CarloNicora\Minimalism\Services\ResourceBuilder;
 
 use CarloNicora\JsonApi\Objects\ResourceObject;
 use CarloNicora\Minimalism\Abstracts\AbstractService;
+use CarloNicora\Minimalism\Interfaces\Cache\Enums\CacheType;
+use CarloNicora\Minimalism\Interfaces\Cache\Interfaces\CacheBuilderInterface;
+use CarloNicora\Minimalism\Interfaces\Cache\Interfaces\CacheInterface;
 use CarloNicora\Minimalism\Services\ResourceBuilder\Interfaces\ResourceableDataInterface;
 use CarloNicora\Minimalism\Services\ResourceBuilder\Interfaces\ResourceBuilderInterface;
 use Exception;
@@ -10,37 +13,79 @@ use Exception;
 class ResourceBuilder extends AbstractService
 {
     /**
+     * @param CacheInterface|null $cache
+     */
+    public function __construct(
+        private ?CacheInterface $cache=null,
+    )
+    {
+        if (!$this->cache->useCaching()){
+            $this->cache = null;
+        }
+    }
+
+    /**
      * @param string $builderClass
      * @param ResourceableDataInterface[] $data
+     * @param CacheBuilderInterface|null $cacheBuilder
      * @return ResourceObject[]
      * @throws Exception
      */
     public function buildResources(
         string $builderClass,
         array $data,
+        ?CacheBuilderInterface $cacheBuilder=null,
     ): array
     {
-        /** @var ResourceBuilderInterface $resourceBuilder */
-        $resourceBuilder = $this->objectFactory->create($builderClass);
+        $response = null;
+        if ($this->cache !== null && $cacheBuilder !== null && ($response = $this->cache->read($cacheBuilder, CacheType::Json)) !== null){
+            $response = unserialize($response, [true]);
+        }
 
-        return $resourceBuilder->buildResources(data: $data);
+        if ($response === null){
+            /** @var ResourceBuilderInterface $resourceBuilder */
+            $resourceBuilder = $this->objectFactory->create($builderClass);
+
+            $response = $resourceBuilder->buildResources(data: $data);
+
+            if ($this->cache !== null && $cacheBuilder !== null) {
+                $this->cache->save($cacheBuilder, serialize($response), CacheType::Json);
+            }
+        }
+
+        return $response;
     }
 
     /**
      * @param string $builderClass
      * @param ResourceableDataInterface $data
+     * @param CacheBuilderInterface|null $cacheBuilder
      * @return ResourceObject
      * @throws Exception
      */
     public function buildResource(
         string $builderClass,
         ResourceableDataInterface $data,
+        ?CacheBuilderInterface $cacheBuilder=null,
     ): ResourceObject
     {
-        /** @var ResourceBuilderInterface $resourceBuilder */
-        $resourceBuilder = $this->objectFactory->create($builderClass);
+        $response = null;
 
-        return $resourceBuilder->buildResource(data: $data);
+        if ($this->cache !== null && $cacheBuilder !== null && ($response = $this->cache->read($cacheBuilder, CacheType::Json)) !== null){
+            $response = unserialize($response, [true]);
+        }
+
+        if ($response === null) {
+            /** @var ResourceBuilderInterface $resourceBuilder */
+            $resourceBuilder = $this->objectFactory->create($builderClass);
+            $response = $resourceBuilder->buildResource(data: $data);
+
+            if ($this->cache !== null && $cacheBuilder !== null) {
+                $this->cache->save($cacheBuilder, serialize($response), CacheType::Json);
+            }
+        }
+
+        return $response;
     }
 
     /**
